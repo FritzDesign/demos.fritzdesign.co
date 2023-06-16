9c61ab385dc7da3091ff56db551ed3f3ccf5baf6
CJ <cjfritz9@gmail.com>
2023-06-16 12:29:01 -0600

    increased default logo size

--- src/components/Showcase/Authentication/Shared/Logo.tsx
+++ src/components/Showcase/Authentication/Shared/Logo.tsx
@@ -6,7 +6,7 @@ interface Props {
   width?: number;
 }
 
-const Logo: React.FC<Props> = ({ width = 60 }) => {
+const Logo: React.FC<Props> = ({ width = 96 }) => {
   return (
     <Stack h='100%' w='100%' alignItems='center' justifyContent='center'>
       <AspectRatio w={width + 'px'} ratio={1}>
